aab45124f7ebf19e322481c34c8bee46af87e1f8
git Hero-masa <maikeru101869@gmail.com>
2021-04-09 23:48:39 +0900

    create login/logout

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,31 @@ use App\Http\Controllers\AuthController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' => ['guest']], function() {
+    // ログインフォーム表示
+    Route::get('/',  'App\Http\Controllers\Auth\AuthController@showLogin')->name('login.show');
+    // Route::get('/',
+    // [AuthController::class, 'showLogin'])
+    // ->name('login.show');
 
-Route::get('/',  'App\Http\Controllers\Auth\AuthController@showLogin')->name('showLogin');
-Route::post('/login',  'App\Http\Controllers\Auth\AuthController@login')->name('login');
+    // ログイン処理
+    Route::post('/login',  'App\Http\Controllers\Auth\AuthController@login')->name('login');
+    // Route::post('login',
+    // [AuthController::class, 'login'])
+    // ->name('login');
+
+});
+
+Route::group(['middleware' => ['auth']], function() {
+    // ホーム画面
+    Route::get('home', function() {
+        return view('home');
+    })->name('home');
+    // ログアウト
+    // Route::post('logout',
+    // [AuthController::class, 'logout'])->name('logout');
+
+    Route::post('logout',  'App\Http\Controllers\Auth\AuthController@logout')->name('logout');
+
+
+});
